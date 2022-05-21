cc909fc4a40445e60b5a47dc74b15a3383a31a85
Harsh Vishwakarma <you@example.com>
2022-05-21 03:36:03 -0700

    complete

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.querySelector(".m-menu");
+
+menuBtn.addEventListener("click", () => {
+  console.log("dgu df dfjsd");
+
+
+
+    mobileMenu.classList.toggle("display");
+    mobileMenu.classList.toggle("mobile-menu");
+
+});  
