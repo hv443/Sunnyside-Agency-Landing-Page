0e9d7e55544cfbe190483869c904ee174772d0fd
Harsh Vishwakarma <you@example.com>
2022-05-23 03:05:09 -0700

    menu change

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const menuBtn = document.getElementById("menuBtn");
 const mobileMenu = document.querySelector(".m-menu");
+const desktopNav = document.querySelectorAll(".desktop-nav li");
+const mobileNav = document.querySelectorAll(".m-menu li");
 
 menuBtn.addEventListener("click", () => {
-  console.log("dgu df dfjsd");
+  mobileMenu.classList.toggle("display");
+  mobileMenu.classList.toggle("mobile-menu");
+});
 
+desktopNav.forEach((element) => {
+  element.addEventListener("click", function clicked() {
+    desktopNav.forEach((e) => {
+      e.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
 
-
-    mobileMenu.classList.toggle("display");
-    mobileMenu.classList.toggle("mobile-menu");
-
-});  
+mobileNav.forEach((element) => {
+  element.addEventListener("click", function Mclicked() {
+    mobileNav.forEach((e) => {
+      e.classList.remove("m-active");
+    });
+    this.classList.add("m-active");
+    console.log("ho");
+  });
+});
